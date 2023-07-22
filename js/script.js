9daed27cab4e4aae28c72c9f1cf51eb081bca9b8
Bartek Jasek <BartekJasek4@gmail.com>
2023-07-22 15:24:31 +0200

    generate list of titles

--- js/script.js
+++ js/script.js
@@ -41,8 +41,49 @@ for (let activeArticle of activeArticles) {
 
 };
 
+
+
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks() {
+  /* [DONE] remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML = '';
+/* find all the articles and save them to variable: articles */
+  const articles = document.querySelectorAll(optArticleSelector);
+  let html = '';
+  
+/* for each article */
+
+  for (let article of articles) {
+    /* [DONE] get the article id */
+    const articleId = article.getAttribute('id');
+    console.log('articleId:', articleId);
+
+    /* [DONE] find the title element */
+   const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+    /* [DONE above] get the title from the title element */
+  
+
+    /* [DONE] create HTML of the link */
+  const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+
+    /* [DONE] insert link into titleList */
+ html = html + linkHTML;
+    console.log(html);
+  }
+
+titleList.innerHTML = html;
 const links = document.querySelectorAll('.titles a');
+console.log('linki:', links);
 
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
+
+}
+
+generateTitleLinks();
