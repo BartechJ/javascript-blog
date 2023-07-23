7c779782098ff1ae41dc9eb806db924311b7e3ba
Bartek Jasek <BartekJasek4@gmail.com>
2023-07-23 11:21:44 +0200

    fix problem finally no need help for now

--- js/script.js
+++ js/script.js
@@ -134,7 +134,7 @@ const clickedElement = this;
 const href = clickedElement.getAttribute('href');
   
   /* make a new constant "tag" and extract tag from the "href" constant */
-const tag = href.replace('#tag-', '');
+const tag = clickedElement.textContent;
 console.log(tag);
   /* find all tag links with class active */
 const activeTagLinks = document.querySelectorAll('a.active[href^="#tag-"]');
